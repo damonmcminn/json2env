8d46829f019d2c1fb6deb6a1c53af518f3e68a2b
Damon McMinn <damonrmcminn@gmail.com>
2015-04-06 16:17:36 +0100

    Catch no file argument or non existing file errors

--- json2env.js
+++ json2env.js
@@ -1,16 +1,24 @@
 #! /usr/bin/env node
 
-const fs = require('fs');
-const variable = process.argv[3];
-const file = fs.readFileSync(process.argv[2])
-  .toString()
-  .split('\n')
-  .filter(function(str) {
-    return str.length > 0;
-  })
-  .map(function(str) {
-    return str.trim();
-  })
-  .join('')
-process.stdout.write(file);
+var fs = require('fs');
+var output;
+var message;
+
+try {
+  output = fs.readFileSync(process.argv[2])
+    .toString()
+    .split('\n')
+    .filter(function(str) {
+      return str.length > 0;
+    })
+    .map(function(str) {
+      return str.trim();
+    })
+    .join('')
+} catch (e) {
+  var noFile = (e.name === 'TypeError');
+  output = noFile ? 'No input file' : ('No such file: ' + e.path)
+}
+
+process.stdout.write(output);
 process.exit();
